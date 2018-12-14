7b0df731f897b667b3b25742e2de04420fd62f32
saikumarwa93 <saikumar.wa93@gmail.com>
2018-12-14 15:15:42 -0500

    refactor code

--- CalAvgTemperature/src/com/temperature/avg/AvgTempCalc.java
+++ CalAvgTemperature/src/com/temperature/avg/AvgTempCalc.java
@@ -19,51 +19,39 @@ public class AvgTempCalc {
 	}
 
 	private static void oneSecondDiff(List<String> tempInput) {
-		float var1 = 0;
-		int count = 0,count2=0,count3=0,count4=0;
+		float total =0,total1 = 0,total2=0,total3=0;
+		int count = 0,count1=0,count2=0,count3=0;
+		Formatter fmt = new Formatter();Formatter fmt1 = new Formatter();Formatter fmt2 = new Formatter();Formatter fmt3 = new Formatter();
 		for(int i =0; i<tempInput.size();i++){
 			float val1 = Integer.parseInt(tempInput.get(i).substring(2, 7));
 			float val2 = Integer.parseInt(tempInput.get(i).substring(8, 10));
 			if(val1 >=10000 && val1 <=10999 ){
 				++count;
-				var1 = var1 + val2;
-				Formatter fmt = new Formatter();
-				fmt.format("%.2f", var1/count);
-			    System.out.println(i+"-"+j +":" + fmt);
+				total = total + val2;
+			    
 			}
 			if(val1 >=11000 && val1 <=11999 ){
-				++count2;
-				var1 = var1 + val2;
-				Formatter fmt = new Formatter();
-				fmt.format("%.2f", var1/count);
-			    System.out.println(i+"-"+j +":" + fmt);
+				++count1;
+				total1 = total1 + val2;
+				
 			}
 			if(val1 >=12000 && val1 <=12999 ){
-				++count3;
-				var1 = var1 + val2;
-				Formatter fmt = new Formatter();
-				fmt.format("%.2f", var1/count);
-			    System.out.println(i+"-"+j +":" + fmt);
+				++count2;
+				total2 = total2 + val2;	
+				
 			}
 			if(val1 >=13000 && val1 <=13999 ){
-				++count4;
-				var1 = var1 + val2;
-				Formatter fmt = new Formatter();
-				fmt.format("%.2f", var1/count);
-			    System.out.println(i+"-"+j +":" + fmt);
+				++count3;
+				total3 = total3 + val2;
+				
 			}
+		
 		}
+		System.out.println("10000-10999: " + fmt.format("%.2f", total/count));
+		System.out.println("11000-11999: " + fmt1.format("%.2f", total1/count1));
+		System.out.println("12000-12999: " + fmt2.format("%.2f", total2/count2));
+		System.out.println("13000-13999: " + fmt3.format("%.2f", total3/count3));
 		
 	}
-
-	private static void printValues(float var1, float val2, int i, int j, int count) {
-		++count;
-		var1 = var1 + val2;
-		Formatter fmt = new Formatter();
-		fmt.format("%.2f", var1/count);
-	    System.out.println(i+"-"+j +":" + fmt);
-		
-	}
-
 
 }
